3b60d1b1dbf7a5a36ae7581a29f0fc030a3a0c1b
Aleksandar Bukvic <aleksandar.buk@gmail.com>
2023-12-01 21:24:38 +0100

    AboutMe Update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,10 +6,6 @@ export const AboutMe = () => (
   <div className={`${styles.flexCenter} ${styles.padding}`}>
     <div className={`${styles.boxWidth}`}>
       <h2 className={styles.heading2}>About Me</h2>
-      
-      
-
-
       <p className={`${styles.paragraph} ${styles.padding_p_am}`}>
         Hello! I&apos;m Andjela, a passionate photographer with a deep love for capturing the beauty in people and moments. My journey with photography began as a hobby but quickly evolved into a profound means of storytelling and expression. I specialize in portrait, landscape, street photography, bringing a unique perspective to each shot.
       </p>
@@ -18,7 +14,6 @@ export const AboutMe = () => (
         My philosophy revolves around the power of a photograph to not just preserve a moment, but to also evoke emotion and tell a story beyond words. With a keen eye for detail and a relentless pursuit of the perfect shot, I strive to create images that resonate with emotion and authenticity.
       </p>
 
-      {/* Selfie Image Container */}
       <div className={styles.selfieContainer}>
         <img src={about_me_selfie} alt="Selfie of Andjela" className={styles.selfieStyle} />
       </div>
@@ -30,7 +25,6 @@ export const AboutMe = () => (
       <p className={`${styles.paragraph} ${styles.padding_p_am}`}>
         Whether it&apos;s a fleeting smile, a serene landscape, or the chaotic beauty of urban life, my goal is to bring those scenes to life through my camera. Every photo I take is a piece of my journey, and I&apos;m excited to share it with you.
       </p>
-      {/* Add more content as needed */}
     </div>
   </div>
 );
