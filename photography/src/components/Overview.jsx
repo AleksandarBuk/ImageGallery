da4c33a16a4f0145e04f7f7bb821c48b6f07367e
AleksandarBuk <aleksandar.buk@.gmail.com>
2024-02-22 21:31:18 +0100

    updates

--- photography/src/components/Overview.jsx
+++ photography/src/components/Overview.jsx
@@ -1,20 +1,17 @@
 import styles from "../style";
-import AboutMe from "./AboutMe";
 
 const Overview = () => (
-  <section className={`${styles.fadeIn} grid grid-cols-2 gap-4 py-10 px-5`}>
-    {/* First Column for Approach */}
+  <section className={`${styles.fadeIn} grid sm:grid-cols-2 grid-cols-1 gap-4 py-10 px-5`}>
     <div className={`${styles.aboutMeBackground}`}>
       <h2 className={`${styles.heading2} paragraphMargin`}>Approach</h2>
-      <p className="paragraphMargin paragraphPadding"> I approach each project by merging my creative vision with technical precision, carefully planning shots and mastering lighting and composition to translate the client's goals into impactful visual narratives.</p>
+      <p className="paragraphMargin paragraphPadding">I approach each project by merging my creative vision with technical precision, carefully planning shots and mastering lighting and composition to translate the client's goals into impactful visual narratives.</p>
     </div>
     
-    {/* Second Column for Overview */}
     <div className={`${styles.aboutMeBackground}`}>
-    <h2 className={`${styles.heading2} paragraphMargin`}>Overview</h2>
-    <p className="paragraphMargin paragraphPadding">By staying at the forefront of photographic trends and techniques, I ensure each photograph is a testament to quality, creativity, and emotional depth.</p>
+      <h2 className={`${styles.heading2} paragraphMargin`}>Overview</h2>
+      <p className="paragraphMargin paragraphPadding">By staying at the forefront of photographic trends and techniques, I ensure each photograph is a testament to quality, creativity, and emotional depth.</p>
     </div>
   </section>
 );
 
-export default Overview;  
+export default Overview;
