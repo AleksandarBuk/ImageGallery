7a83576016ca1235a7241341b14bb7c18faa4476
AleksandarBuk <aleksandar.buk@.gmail.com>
2024-02-24 19:24:27 +0100

    Translation

--- photography/src/components/Overview.jsx
+++ photography/src/components/Overview.jsx
@@ -3,12 +3,12 @@ import styles from "../style";
 const Overview = () => (
   <section className={`${styles.fadeIn} grid sm:grid-cols-2 grid-cols-1 gap-4 py-10 px-5`}>
     <div className={`${styles.aboutMeBackground} hover:shadow-lg hover:border-blue-500 border-2 border-transparent transition-all`}>
-      <h2 className={`${styles.heading2} paragraphMargin`}>Approach</h2>
+      <h2 className={`${styles.heading2} paragraphMargin`}>Pristup</h2>
       <p className="paragraphMargin paragraphPadding">Pristupam svakom projektu spajanjem moje kreativne vizije sa tehničkom preciznošću, pažljivo planirajući snimke, ovladavajući osvetljenjem i kompozicijom kako bih provela ciljeve klijenta u snažne vizuelne narative.</p>
     </div>
     
     <div className={`${styles.aboutMeBackground} hover:shadow-lg hover:border-blue-500 border-2 border-transparent transition-all`}>
-      <h2 className={`${styles.heading2} paragraphMargin`}>Overview</h2>
+      <h2 className={`${styles.heading2} paragraphMargin`}>Pregled</h2>
       <p className="paragraphMargin paragraphPadding">Ostajući u prvom planu fotografskih trendova i tehnika, osiguravam da je svaka fotografija dokaz kvaliteta, kreativnosti i emocionalne dubine.</p>
     </div>
   </section>
